3449c497aeec96f5576c4533e40136809bf5b702
Rafael J. Staib <rstaib@derklang.com>
2013-05-22 23:03:49 +0200

    Add a new feature note

--- jquery.steps.js
+++ jquery.steps.js
@@ -27,6 +27,7 @@
  * - Dynamic step update (setStepContent(0, { title: "", content: "" }))
  * - Jump from any page to a specific step (via uri hash tag test.html#steps-uid-1-3)
  * - Add Swipe gesture for devices that support touch
+ * - Allow clicking on the next step even if it is disabled (so that people can decide whether they use prev button or the step button next to the current step)
  *
  */
 
