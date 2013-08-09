35898ba9fc8641fc1b369b58d31fd3325061cddd
Rafael J. Staib <rstaib@derklang.com>
2013-08-10 01:03:19 +0200

    Add fix for the viewport issue on mobie ie10

---
+++ V2/scripts/viewport-fix-mobile-ie10.js
@@ -0,0 +1,6 @@
+﻿if (navigator.userAgent.match(/IEMobile\/10\.0/))
+{
+    var msViewportStyle = document.createElement("style");
+    msViewportStyle.appendChild(document.createTextNode("@-ms-viewport{width:auto!important}"));
+    document.getElementsByTagName("head")[0].appendChild(msViewportStyle);
+}
