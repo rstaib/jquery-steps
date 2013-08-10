1eab5dec4dff8d4a0e3c033bc7165ed0b5647b0f
Rafael J. Staib <rstaib@derklang.com>
2013-08-11 01:52:21 +0200

    Add a lot css improvements

---
+++ JSteps/obj/Release/AspnetCompileMerge/TempBuildDir/Scripts/social.js
@@ -0,0 +1,5 @@
+// Twitter Star Button
+!function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https'; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = p + '://platform.twitter.com/widgets.js'; fjs.parentNode.insertBefore(js, fjs); } }(document, 'script', 'twitter-wjs');
+
+// Facebook Like Button
+(function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1"; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk'));
