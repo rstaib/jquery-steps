a5095c6fd412756445887acf5a494fd090dff70d
Rafael J. Staib <rstaib@derklang.com>
2013-08-10 21:31:21 +0200

    Move source code to a MVC project and start with subpage design

---
+++ JSteps/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace JSteps
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Scripts/Base")
+                .Include("~/Scripts/jquery-{version}.js")
+                .Include("~/Scripts/bootstrap.js")
+                .Include("~/Scripts/viewport-fix-mobile-ie10.js")
+                .Include("~/Scripts/social.js")
+                .Include("~/Scripts/tracking.js"));
+
+            bundles.Add(new ScriptBundle("~/Scripts/html5shiv")
+                .Include("~/Scripts/html5shiv.js"));
+
+            bundles.Add(new StyleBundle("~/Content/Base")
+                .Include("~/Content/bootstrap.css")
+                .Include("~/Content/viewport-fix-mobile-ie10.css")
+                .Include("~/Content/custom.css"));
+        }
+    }
+}
