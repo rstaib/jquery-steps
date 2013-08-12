8e6f154c49ae657e71cfb9089fadcaea92f8b746
Rafael Staib <rstaib@derklang.com>
2013-08-12 18:08:33 +0200

    Add more text

--- JSteps/App_Start/BundleConfig.cs
+++ JSteps/App_Start/BundleConfig.cs
@@ -8,22 +8,34 @@ namespace JSteps
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/Scripts/Base")
+            bundles.Add(new ScriptBundle("~/Scripts/html5shiv")
+                .Include("~/Scripts/html5shiv.js"));
+
+            bundles.Add(GetBaseScripts("~/Scripts/Base"));
+            bundles.Add(GetBaseScripts("~/Scripts/Extended")
+                .Include("~/Scripts/prettify.js"));
+
+            bundles.Add(GetBaseStyles("~/Content/Base"));
+            bundles.Add(GetBaseStyles("~/Content/Extended")
+                .Include("~/Content/prettify.css"));
+        }
+
+        private static Bundle GetBaseScripts(string virtualPath)
+        {
+            return new ScriptBundle(virtualPath)
                 .Include("~/Scripts/jquery-{version}.js")
                 .Include("~/Scripts/bootstrap.js")
                 .Include("~/Scripts/viewport-fix-mobile-ie10.js")
                 .Include("~/Scripts/social.js")
-                .Include("~/Scripts/tracking.js")
-                .Include("~/Scripts/prettify.js"));
+                .Include("~/Scripts/tracking.js");
+        }
 
-            bundles.Add(new ScriptBundle("~/Scripts/html5shiv")
-                .Include("~/Scripts/html5shiv.js"));
-
-            bundles.Add(new StyleBundle("~/Content/Base")
+        private static Bundle GetBaseStyles(string virtualPath)
+        {
+            return new StyleBundle(virtualPath)
                 .Include("~/Content/bootstrap.css")
                 .Include("~/Content/viewport-fix-mobile-ie10.css")
-                .Include("~/Content/custom.css")
-                .Include("~/Content/prettify.css"));
+                .Include("~/Content/custom.css");
         }
     }
 }
