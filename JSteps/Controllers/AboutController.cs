c2b1e5a2ac7e4bf4738d3b5bd35ea8cc6abb7488
Rafael J. Staib <rstaib@derklang.com>
2013-08-13 00:01:27 +0200

    Add more and more text and of course improved css

---
+++ JSteps/Controllers/AboutController.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JSteps.Controllers
+{
+    public class AboutController
+        : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- JSteps/obj/Release/Package/PackageTmp/Scripts/social.js
+++ JSteps/obj/Release/Package/PackageTmp/Scripts/social.js
@@ -3,3 +3,6 @@
 
 // Facebook Like Button
 (function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1"; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk'));
+
+// Google +1 Button
+(function () { var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true; po.src = 'https://apis.google.com/js/plusone.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s); })();
