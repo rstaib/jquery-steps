aceee85c69ec31c856fca8cdc12735e8806d0fab
Rafael J. Staib <rstaib@derklang.com>
2013-08-13 10:24:34 +0200

    Add donate button

---
+++ JSteps/Controllers/DonationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JSteps.Controllers
+{
+    public class DonationController
+        : Controller
+    {
+        public ActionResult Success()
+        {
+            return View();
+        }
+    }
+}
