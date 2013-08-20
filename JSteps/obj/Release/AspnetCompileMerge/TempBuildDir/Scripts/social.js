b47cd9b7300eed636ea4d74e3e9a7e3f88dcac1e
Rafael J. Staib <rstaib@derklang.com>
2013-08-20 21:44:58 +0200

    Clean jSteps folder

--- JSteps/obj/Release/AspnetCompileMerge/TempBuildDir/Scripts/social.js
+++
@@ -1,8 +0,0 @@
-// Twitter Star Button
-!function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https'; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = p + '://platform.twitter.com/widgets.js'; fjs.parentNode.insertBefore(js, fjs); } }(document, 'script', 'twitter-wjs');
-
-// Facebook Like Button
-(function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1"; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk'));
-
-// Google +1 Button
-(function () { var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true; po.src = 'https://apis.google.com/js/plusone.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s); })();
--- JSteps/obj/Release/AspnetCompileMerge/TempBuildDir/Scripts/viewport-fix-mobile-ie10.js
+++
@@ -1,6 +0,0 @@
-﻿if (navigator.userAgent.match(/IEMobile\/10\.0/))
-{
-    var msViewportStyle = document.createElement("style");
-    msViewportStyle.appendChild(document.createTextNode("@-ms-viewport{width:auto!important}"));
-    document.getElementsByTagName("head")[0].appendChild(msViewportStyle);
-}
