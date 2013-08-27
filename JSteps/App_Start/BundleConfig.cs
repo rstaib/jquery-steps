2dad3e90f517a309cb209f0ad5994161382c83a3
Rafael J. Staib <rstaib@derklang.com>
2013-08-27 20:34:10 +0200

    Solve a problem on the example page

--- JSteps/App_Start/BundleConfig.cs
+++ JSteps/App_Start/BundleConfig.cs
@@ -1,4 +1,6 @@
-﻿using System.Web;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Web;
 using System.Web.Optimization;
 
 namespace JSteps
@@ -8,16 +10,21 @@ namespace JSteps
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            BundleTable.EnableOptimizations = true;
+
             bundles.Add(new ScriptBundle("~/Scripts/html5shiv")
                 .Include("~/Scripts/html5shiv.js"));
 
-            bundles.Add(GetBaseScripts("~/Scripts/Base"));
+            bundles.Add(GetBaseScripts("~/Scripts/Base")
+                .AddFooterScripts());
             bundles.Add(GetBaseScripts("~/Scripts/Extended")
-                .Include("~/Scripts/prettify.js"));
+                .Include("~/Scripts/prettify.js")
+                .AddFooterScripts());
             bundles.Add(GetBaseScripts("~/Scripts/Examples")
                 .Include("~/Scripts/jquery.steps.js")
                 .Include("~/Scripts/jquery.validate.js")
-                .Include("~/Scripts/prettify.js"));
+                .Include("~/Scripts/prettify.js")
+                .AddFooterScripts());
 
             bundles.Add(GetBaseStyles("~/Content/Base"));
             bundles.Add(GetBaseStyles("~/Content/Extended")
@@ -29,20 +36,34 @@ namespace JSteps
 
         private static Bundle GetBaseScripts(string virtualPath)
         {
-            return new ScriptBundle(virtualPath)
+            return new ScriptBundle(virtualPath) { Orderer = new BundleOrderer() }
                 .Include("~/Scripts/jquery-{version}.js")
-                .Include("~/Scripts/bootstrap.js")
-                .Include("~/Scripts/viewport-fix-mobile-ie10.js")
-                .Include("~/Scripts/social.js")
-                .Include("~/Scripts/tracking.js");
+                .Include("~/Scripts/bootstrap.js");
         }
 
         private static Bundle GetBaseStyles(string virtualPath)
         {
-            return new StyleBundle(virtualPath)
+            return new StyleBundle(virtualPath) { Orderer = new BundleOrderer() }
                 .Include("~/Content/bootstrap.css")
-                .Include("~/Content/viewport-fix-mobile-ie10.css")
                 .Include("~/Content/custom.css");
+        }
+    }
+
+    public static class BundleExtensions
+    {
+        public static Bundle AddFooterScripts(this Bundle bundle)
+        {
+            return bundle.Include("~/Scripts/social.js")
+                .Include("~/Scripts/tracking.js");
+        }
+    }
+
+    public class BundleOrderer
+        : IBundleOrderer
+    {
+        public IEnumerable<BundleFile> OrderFiles(BundleContext context, IEnumerable<BundleFile> files)
+        {
+            return files;
         }
     }
 }
